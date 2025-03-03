cf8fc5b4ad9b8bd18704014eea133f5d518f95de
Algot2 <156318062+Algot2@users.noreply.github.com>
2025-03-03 11:06:31 +0100

    Hellf

--- Assets/Scripts/RatBase.cs
+++ Assets/Scripts/RatBase.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.Splines;
+using UnityEngine.UI;
 
 public class RatBase : MonoBehaviour
 {
     public SplineAnimate spline;
 
-    public float spede, health;
+    public float spede, health, damitsh;
+    public Slider temp;
 
     void Start() { spline.MaxSpeed = spede; }
     public virtual void IsDedFrFr()
@@ -13,11 +15,18 @@ public class RatBase : MonoBehaviour
         Destroy(gameObject);
     }
 
+    public virtual void AtEnd()
+    {
+        temp.value -= damitsh;
+        Destroy(gameObject);
+    }
+
     public void RunDefaltCode()
     {
         if (health <= 0) IsDedFrFr();
+        if (spline.normalizedTime > 0.99f) AtEnd();
     }
 
 
-
+   
 }
