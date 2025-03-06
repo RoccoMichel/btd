08cabaee55ebc7d255cce65b4fd9ba5cd86ce434
RoccoMichel <122682577+RoccoMichel@users.noreply.github.com>
2025-03-06 13:21:26 +0100

    NEW BUILD
    just for me
    -Rocco

--- Assets/Scripts/CatBase.cs
+++ Assets/Scripts/CatBase.cs
@@ -130,7 +130,6 @@ public class CatBase : MonoBehaviour
             if (ani != null)
                 ani.SetTrigger(aniShootParam);
         }
-
     }
 
     void OnMouseOver()
@@ -166,12 +165,15 @@ public class CatBase : MonoBehaviour
     public void ShowUpgrade()
     {
         if (!canAtacke) return;
-        if (upgradeMenu != null) 
-        { 
-            upgradeMenu.KillYourSelf(); 
-            return; 
-        } // Don't create dublicate upgradeMenus if spam clicking
 
+        // Kill all other menus
+        if (GameObject.FindGameObjectsWithTag("UpgradeMenu") != null) 
+        {
+            GameObject[] unwantedMenus = GameObject.FindGameObjectsWithTag("UpgradeMenu");
+            foreach (GameObject unwantedMenu in unwantedMenus) unwantedMenu.GetComponent<CatUpgrade>().KillYourSelf();
+        }
+
+        // Create new menu
         upgradeMenu = Instantiate(Resources.Load("Upgrade Menu").GameObject().GetComponent<CatUpgrade>());
         upgradeMenu.gameObject.transform.position = transform.position + Vector3.up * 6; // Position above the cat
         upgradeMenu.cat = this;
