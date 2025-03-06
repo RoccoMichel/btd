20f87c6b1b4558e0950f2b91dffd7105a8ae5fdc
Bolt_Bug <Charlie.stalhammar@gmail.com>
2025-03-06 11:06:30 +0100

    fixed not loading level on second try

--- Assets/Scripts/Useful/LoadLogic.cs
+++ Assets/Scripts/Useful/LoadLogic.cs
@@ -5,30 +5,44 @@ public class LoadLogic : MonoBehaviour
 {
     public static void LoadSceneByName(string sceneName)
     {
+        Time.timeScale = 1;
+
         SceneManager.LoadScene(sceneName);
     }
     public static void LoadNextInBuild()
     {
+        Time.timeScale = 1;
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public static void LoadPreviousInBuild()
     {
+        Time.timeScale = 1;
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
     public static void LoadSceneByNumber(int sceneNumber)
     {
+        Time.timeScale = 1;
+
         SceneManager.LoadScene(sceneNumber);
     }
     public static void ReloadScene()
     {
+        Time.timeScale = 1;
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     public static void LoadSceneByPlayerPrefInt(string tag)
     {
+        Time.timeScale = 1;
+
         SceneManager.LoadScene(PlayerPrefs.GetInt(tag));
     }
     public static void LoadSceneByPlayerPrefString(string tag)
     {
+        Time.timeScale = 1;
+
         SceneManager.LoadScene(PlayerPrefs.GetString(tag));
     }
     public static void QuitGame()
