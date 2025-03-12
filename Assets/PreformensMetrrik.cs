13614e1aa1a7130bcfd7988f0be64d6d79365277
Algot2 <156318062+Algot2@users.noreply.github.com>
2025-03-12 12:06:23 +0100

    l

---
+++ Assets/PreformensMetrrik.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreformensMetrrik : MonoBehaviour
+{
+    public bool prefomMetrick;
+    public int dataPonts;
+    public float scale;
+    public float[] data = new float[10];
+    public LineRenderer line;
+    public List<Transform> TragetFrameRate;
+    void Start()
+    {
+        data = new float[dataPonts];
+        line.positionCount = dataPonts;
+    }
+    void draGrafe()
+    {
+        Vector3 pos = transform.position + transform.forward;
+        for (int i = 0; i < data.Length; i++)
+        {
+            line.SetPosition(i, new Vector3(( (i*-0.5f) / data.Length), data[i]*-scale, 0));
+        }
+        
+        for (int i = 0; i < TragetFrameRate.Count; i++)
+        {
+            TragetFrameRate[i].localPosition = Vector3.right * -.25f 
+                + Vector3.up * -scale * ((1/60f) + (1/100f)*i);
+
+            if (i != 0) TragetFrameRate[i].localPosition += Vector3.right * 0.02f;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F3)) prefomMetrick = !prefomMetrick;
+
+        if (prefomMetrick)
+        {
+            line.gameObject.SetActive(true);
+
+            for (int i = 1; i < data.Length; i++)
+            {
+                if (i == data.Length - 1) data[i] = Time.deltaTime;
+                data[i - 1] = data[i];
+            }
+            draGrafe();
+        }
+        else
+        {
+            line.gameObject.SetActive(false);
+        }
+    }
+}
